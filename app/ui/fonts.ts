855ca753b0e8d88ec8d78eed5e29092faf206c03
lisongfeng <lsf.email@gmail.com>
2025-04-06 07:27:49 +0800

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,16 @@
+import exp from 'constants';
+import { Inter, Lusitana } from 'next/font/google';
+
+
+export const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  display: 'swap',
+})
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  variable: '--font-lusitana',
+  display:'swap',
+  weight: ['400', '700'],
+})
